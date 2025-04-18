020553652a0e95c693b4fd1a89467d5fc8ba2ba1
aaronPro <aaroncascino@gmail.com>
2025-04-17 21:19:24 -0300

    intentar cambiar el codigo a funcional

--- lista de tareas/agregar_mod.php
+++ lista de tareas/agregar_mod.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
 <?php
 //Set timezone to Argentina
 date_default_timezone_set('America/Argentina/Buenos_Aires');  
@@ -17,14 +8,9 @@ $descripcion=$_POST['Descripcion'];
 $esta_finalizado=$_POST['Esta_Finalizado'];
 $fecha_inicial=$_POST['Fecha_Inicial'];
 $fecha_final=$_POST['Fecha_Final'];
-$query="INSERT INTO `tareas` (`id`, `Tarea`, `Descripcion`, `Esta_Finalizado`, `Fecha_Final`, `Fecha_Inicial`, `Fecha_Creacion`) VALUES (NULL, '".$tarea."', '".$descripcion."', '".$esta_finalizado."', '".$fecha_final."', '".$fecha_inicial."', '".date('Y-m-d H:i:s')."');";
+$query="INSERT INTO `tareas` (`Tarea`, `Descripcion`, `Esta_Finalizado`, `Fecha_Final`, `Fecha_Inicial`, `Fecha_Creacion`) VALUES (NULL, '".$tarea."', '".$descripcion."', '".$esta_finalizado."', '".$fecha_final."', '".$fecha_inicial."', '".date('Y-m-d H:i:s')."');";
 $res=mysqli_query($connection,$query);
-?>
-<h1>Agregado Con Exito</h1>
 
-<head>
-    <meta http-equiv="refresh"
-    content="0;url=./lista.php">
-</head>
-</body>
-</html>
+header("Location: lista.php");
+exit();
+?>
